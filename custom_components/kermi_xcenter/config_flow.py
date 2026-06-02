1b505407108c1785eafec71fb5c5fe4f0b541c0d
Hendrik <haufmkolk@me.com>
2026-06-02 23:51:55 +0200

    Fixes

--- custom_components/kermi_xcenter/config_flow.py
+++ custom_components/kermi_xcenter/config_flow.py
@@ -1,35 +1,28 @@
 import voluptuous as vol
 from homeassistant import config_entries
 
-from .oauth import KermiOAuth
-
 DOMAIN = "kermi_xcenter"
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("installation_id"): str,
+        vol.Required("access_token"): str,
+        vol.Required("refresh_token"): str,
+    }
+)
+
 
 class KermiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        oauth = KermiOAuth()
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Kermi X-Center",
+                data=user_input,
+            )
 
-        verifier, challenge = oauth.generate_pkce()
-
-        self.context["verifier"] = verifier
-        self.context["state"] = "kermi_state"
-
-        redirect_uri = self.hass.config.api.base_url + "/auth/external/callback"
-
-        url = oauth.build_auth_url(
-            redirect_uri=redirect_uri,
-            state="kermi_state",
-            challenge=challenge,
-        )
-
-        self.context["redirect_uri"] = redirect_uri
-
-        return self.async_external_step(step_id="auth", url=url)
-
-    async def async_step_auth(self, user_input):
-        return self.async_create_entry(
-            title="Kermi X-Center",
-            data=user_input,
+        return self.async_show_form(
+            step_id="user",
+            data_schema=DATA_SCHEMA,
         )
