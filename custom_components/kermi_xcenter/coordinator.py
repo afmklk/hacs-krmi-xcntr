d65ad8cb2a821e8bebe613589ebdd92981011742
Hendrik <haufmkolk@me.com>
2026-06-04 18:57:36 +0200

    Fixes

--- custom_components/kermi_xcenter/coordinator.py
+++ custom_components/kermi_xcenter/coordinator.py
@@ -1,11 +1,16 @@
 from datetime import timedelta
+import logging
+
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
 
 class KermiCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, api, installation_id):
         super().__init__(
             hass,
-            logger=__name__,
+            logger=_LOGGER,
             name="kermi_xcenter",
             update_interval=timedelta(seconds=30),
         )
