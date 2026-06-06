b2bcd02f378288b59aacc7399bf28148957e313f
Hendrik <haufmkolk@me.com>
2026-06-07 00:54:23 +0200

    Update config_flow.py

--- custom_components/kermi_xcenter/config_flow.py
+++ custom_components/kermi_xcenter/config_flow.py
@@ -125,11 +125,7 @@ class KermiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
-
 class KermiOptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(
